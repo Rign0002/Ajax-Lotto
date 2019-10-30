03a4b44cda0b8b1451294d76f3adaefc0906d7e1
Justin <rign0002@algonquinlive.com>
2019-10-30 13:00:37 -0400

    Updates

---
+++ precache-manifest.de083455de0b741a6efd04eac8251019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4d4ea571a3ade1bf9269d7e74cf715",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e653b9010706c3e60844",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b740644e9421eaa78f61",
+    "url": "/static/js/2.40db384c.chunk.js"
+  },
+  {
+    "revision": "e653b9010706c3e60844",
+    "url": "/static/js/main.959c5668.chunk.js"
+  },
+  {
+    "revision": "0b067edb616103c357c1",
+    "url": "/static/js/runtime-main.1837686c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
